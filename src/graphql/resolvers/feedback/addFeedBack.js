cc9c5ffa06bb9246ac48ec4e6913610c8555176d
mudakikwa <mudakikwastory@gmail.com>
2021-05-27 09:23:07 +0200

    fniished add feedback api

--- src/graphql/resolvers/feedback/addFeedBack.js
+++ src/graphql/resolvers/feedback/addFeedBack.js
@@ -3,14 +3,14 @@ import FeedBack from '../../../database/models/feedback';
 module.exports = {
   AddFeedBack: async (args) => {
     try {
-      const { feedback,userId } = args.codeData;
+      const { feedback , userId } = args.feedBackData;
       const existingFeedBack = await FeedBack.findOne({
-        userId,feedback
+        userId , feedback
       });
       if (existingFeedBack) {
         throw new Error('This FeedBack already exist');
       } else {
-        const newFeedBack = new Code({
+        const newFeedBack = new FeedBack({
           userId,
           feedback,
         });
